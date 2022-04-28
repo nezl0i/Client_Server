5ee1c1ea6ffa9f795c08d6b92935b63ffab41439
nezl0i <nezl0in@gmail.com>
2022-04-28 15:45:08 +0300

    Added example_5. Create logging.

---
+++ app_example_5/jimm/client.py
@@ -0,0 +1,71 @@
+import sys
+import json
+import socket
+import time
+import argparse
+import logging
+import logs.client_log_config
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.utils import get_message, send_message
+
+CLIENT_LOGGER = logging.getLogger('client')
+
+
+def check_port(port):
+    if port not in range(1024, 65536):
+        CLIENT_LOGGER.critical(f'Ошибка запуска портом с портом {port}. Допустимый диапазон портов от 1024 до 65535.')
+        sys.exit(1)
+    return port
+
+
+def create_presence(account_name='Guest'):
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
+    return out
+
+
+def process_ans(message):
+    CLIENT_LOGGER.debug(f'Обработка сообщения от сервера: {message}')
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+
+def main():
+    parser = argparse.ArgumentParser(description="JIM Client_server application")
+
+    parser.add_argument("ip", nargs='?', default=DEFAULT_IP_ADDRESS, help='Server IP')
+    parser.add_argument("port", nargs='?', default=DEFAULT_PORT, type=int, help='Server port')
+    args = parser.parse_args()
+
+    server_port = check_port(args.port)
+    server_address = args.ip
+    CLIENT_LOGGER.info(f'Клиент запущен с параметрами: {server_address}:{server_port}')
+
+    try:
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = create_presence()
+        send_message(transport, message_to_server)
+        CLIENT_LOGGER.info(f'Отправлен запрос на сервер {message_to_server}')
+        answer = process_ans(get_message(transport))
+        transport.close()
+        CLIENT_LOGGER.info(f'Получен ответ от сервера {answer}')
+    except ConnectionRefusedError:
+        CLIENT_LOGGER.critical(f'Не удалось подключиться к серверу {server_address}:{server_port}, '
+                               f'конечный компьютер отверг запрос на подключение.')
+    except (ValueError, json.JSONDecodeError):
+        CLIENT_LOGGER.error(f'Ошибка декодирования JSON пакета от {server_address}.')
+
+
+if __name__ == '__main__':
+    main()
